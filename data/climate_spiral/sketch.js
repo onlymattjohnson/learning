19aff6faa3f9c2cc46461e9f744f631f230f0917
Matt Johnson <mjohn23@gmail.com>
2023-07-17 15:33:37 -0500

    add 1° circle

--- data/climate_spiral/sketch.js
+++ data/climate_spiral/sketch.js
@@ -14,12 +14,20 @@ function setup() {
 function draw() {
   background(0);
   translate(width / 2, height / 2);
-  noFill();
+  
   stroke(255);
-  strokeWeight(4);
-
+  strokeWeight(2);
+  noFill();
   circle(0, 0, 100);
   fill(255);
   noStroke();
   text("0°", 54, 0);
+
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  circle(0, 0, 300);
+  fill(255);
+  noStroke();
+  text("1°", 154, 0);
 }
